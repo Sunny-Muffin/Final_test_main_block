871836d4ff72cf84fc3f5735985d39f578f6fdaf
Ivan Afanasev <vanuk@bk.ru>
2023-04-18 18:41:44 +0500

    made some minor changes

--- Test_task/Program.cs
+++ Test_task/Program.cs
@@ -13,7 +13,7 @@ string GetUserInput()
     string txtInput = Console.ReadLine();
     while(txtInput == null)
     {
-        Console.WriteLine("Please enter at least some symbols");
+        Console.WriteLine("Please enter at least some symbols: ");
         txtInput = Console.ReadLine();
     }
     return txtInput;
@@ -21,7 +21,7 @@ string GetUserInput()
 
 int CountWords(string input)
 {
-    int count = 1; // слов больше, чем пробелов на 1
+    int count = 1; // we consider that there is one word more than there are spaces
     for(int i = 0; i < input.Length; i++)
     {
         if(input[i] == ' ')
@@ -62,8 +62,15 @@ string[] MakeArrayFromString(int lenght, string input)
 // Method below makes new array from elements, that have 3 or less symbols
 string[] MakeNewArray(string[] array)
 {
-    string[] newArray = new string[CountThreeSymbolWords(array)];
     int j = 0;
+    int countWords = CountThreeSymbolWords(array);
+    string[] newArray = new string[countWords];
+    
+    if (countWords == 0)
+    {
+        Console.WriteLine("There are no words with 3 or less symbols");
+        return newArray;
+    }
 
     for (int i = 0; i < array.Length; i++)
     {
